a1e74ec8be8d519b1529babc83f944ed49d26ac2
Celestial Phineas <hanghang0713@hotmail.com>
2017-02-11 21:19:21 +0800

    Typewriter refactoring.

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,25 +1,37 @@
 var typewriter =
 {
-    baseString: 'Under construction...',
     DOMElement: document.getElementById('uc'),
+    baseString: 'Under construction...',
     typing: true,
     cursor: 0,
     frameWidth: 10,
     currentFrame: 0,
     forwardShifting: [],
-    backwardShifting: 1000,
-    init: function()
+    backwardShifting: 100,
+    init: function(
+        _DOMElement,
+        _baseString,
+        _frameWidth,
+        forwardShiftingLowerBound,
+        forwardShiftingUpperBound,
+        backwardShiftingValue
+    )
     {
         var i;
         this.typing = true;
         this.cursor = 0;
-        this.frameWidth = 10;
+        this.frameWidth = _frameWidth;
         this.currentFrame = 0;
-        this.forwardShifting[0] = Math.random() * (500 - 100) + 100;
+        this.DOMElement = _DOMElement;
+        this.baseString = _baseString;
+        this.forwardShifting[0] = Math.random() *
+            (forwardShiftingUpperBound - forwardShiftingLowerBound)
+            + forwardShiftingLowerBound;
+        this.backwardShifting = backwardShiftingValue;
         for(i = 1; i < this.baseString.length; i++)
         {
             this.forwardShifting[i] = this.forwardShifting[i - 1]
-                + Math.random() * (500 -100) + 100;
+                + Math.random() * (forwardShiftingUpperBound -100) + 100;
         }
     },
     shift: function()
@@ -28,42 +40,30 @@ var typewriter =
         if(this.typing)
         {
             for(i = 0; i < this.baseString.length; i++)
-            {
-                if(this.forwardShifting[i] > this.currentFrame) break;
-            }
+                if(this.currentFrame < this.forwardShifting[i]) break;
+            this.cursor = i;
             if(this.cursor === this.baseString.length)
             {
-                this.cursor = this.baseString.length - 1;
                 this.typing = false;
-                this.currentFrame = this.baseString.length * this.frameWidth;
+                this.currentFrame = this.backwardShifting * this.baseString.length;
             }
-            else
-            {
-                this.cursor = i;
-                this.currentFrame += this.frameWidth;
-            }
+            else this.currentFrame += this.frameWidth;
         }
         else
         {
-            for(i = this.baseString.length - 1; i >= 0; i--)
+            for(i = this.baseString.length; i >= 0; i--)
+                if(this.currentFrame > this.backwardShifting * (i + 1)) break;
+            this.cursor = i;
+            if(i === -1)
             {
-                if(i * this.backwardShifting < this.currentFrame) break;
-            }
-            if(this.cursor === -1)
-            {
-                this.cursor = 0;
                 this.typing = true;
                 this.currentFrame = 0;
             }
-            else
-            {
-                this.cursor = i;
-                this.currentFrame -= this.frameWidth;
-            }
+            else this.currentFrame -= this.frameWidth;
         }
-        this.DOMElement.innerText = this.cursor;
+        this.DOMElement.innerText = this.baseString.substr(0, this.cursor);
     }
 }
 
-typewriter.init();
+typewriter.init(document.getElementById('uc'), 'Under construction...', 10, 100, 200, 50);
 setInterval('typewriter.shift()', 10);
